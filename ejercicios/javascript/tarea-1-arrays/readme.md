fb0e50d2f1c373c2dc389f008114b8ac8c395421
Jerónimo Cardona <jeronimocardona70@gmail.com>
2023-10-26 18:12:31 -0500

    Ejericio 1 arrays

---
+++ ejercicios/javascript/tarea-1-arrays/readme.md
@@ -0,0 +1,2 @@
+Ejercicio 1
+Crear un arreglo (temática libre), el cual sea recorrido con la función map() e imprimir por consola cada uno de sus elementos.
---
+++ ejercicios/javascript/tarea-1-arrays/script.js
@@ -0,0 +1,14 @@
+const dinosaurios = [
+    't-rex',
+    'spinosaurus',
+    'triceraptos',
+    'pachycefalosaurus',
+    'pteranodon'
+];
+
+dinosaurios.map((Element) => {
+    console.log (`${Element}`);
+})
+
+
+
